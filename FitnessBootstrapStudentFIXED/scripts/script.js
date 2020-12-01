dfcef66aaba6f7464c6c4022d9cdda8c5a58889a
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2020-11-30 17:41:40 -0700

    FitnessBootstrapStudentFIXED
    Monday assignment called "FitnessBootstrapStudent" due on 11-30-2020. "FIXED" was added because an HTML check was done and errors needed to be fixed and I had to re-upload. Previous errors were fixed.

---
+++ FitnessBootstrapStudentFIXED/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Emma Wood
+    File Name: script.js
+    Date: 11-30-2020
+*/
+
+//JQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
