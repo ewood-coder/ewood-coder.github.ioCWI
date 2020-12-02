2d17c9a06549a4e4e47f9e3362caa39fa944471a
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2020-12-02 13:07:09 -0700

    WildRescuesBootstrap
    Wednesday assignment called "Wild Rescues" due/completed on 12-2-2020

---
+++ WildRescuesBootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Emma wood
+    Date: 12-2-2020
+    File Name: script.js
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
