04c9fb507a74dc46c5496e34e0b804adc1db4258
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-01-25 11:51:31 -0700

    Ch4Ex2
    Monday assignment Chapter 4 Exercise 2 completed on Monday

--- future_value/future_value.js
+++ future_value/future_value.js
@@ -1,86 +1,89 @@
 "use strict";
 
-let again = "y"
-do { /* <-- add a do-while statement to the applica-tion like the one for the MPG application  */
-    
-    // get investment amount - loop until user enters a number
-    let investment = 0;
-    do {
-        investment = parseFloat(
-            prompt("Enter investment amount as xxxxx.xx", 10000));
-    }
-    while ( isNaN(investment)  || investment <= 0); /* <-- Update the condition for the do-while statement for each user entry  */
+const $ = selector => document.querySelector(selector);
 
-    
-    // get interest rate - loop until user enters a number
-    let rate = 0;
-    do {
-        rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
-    }
-    while ( isNaN(rate) || rate <= 0 || rate >= 15); /* <-- Update the condition for the do-while statement for each user entry  */
-
-    
-    // get number of years - loop until user enters a number
-    let years = 0;
-    do {
-        years = parseInt(prompt("Enter number of years", 10));
-    }
-    while ( isNaN(years) || years <= 0); /* <-- Update the condition for the do-while statement for each user entry  */
-    
-    
-    document.write(`<h4>Investment amount = ${investment}
-    Interest rate = ${rate} Years = ${years}</h4>`);
-
-    
-    // calulate future value
+/* Create an arrow function named calculateFV(). 
+   investment amount, interest rate, and number of years. */
+const calculateFV = (investment, rate, years) => {
     let futureValue = investment;
+    
     for (let i = 1; i <= years; i++ ) {
-        /* futureValue = futureValue + (futureValue * rate / 100); */
-        const interest = futureValue * rate / 100;
-        futureValue = futureValue + interest;
-
-        document.write(`<p>Year=${i} Interest=${interest.toFixed(2)}
-        Value=${futureValue.toFixed(2)}</p>`);
+        futureValue += futureValue * rate / 100;
     }
-    again = prompt("Repeat entries? (y/n)", "y");
-}
-while (again == "y");
 
-/*====================
-    display results
-======================*/
-document.write(html);
+    futureValue = futureValue.toFixed(2);
+    return futureValue;
+};
 
 
+/* Create an arrow function named processEntries() that gets the 
+   user entries with no data validation.  */
+// const processEntries = () => {
+ //   const investment = parseFloat($("#investment").value);
+ //   const rate = parseFloat($("#rate").value);
+ //   const years = parseInt($("#years").value);
 
-/*=============================
-        Unecessary Code
-===============================*/
-/* document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
-document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
-document.write(`<p><label>Years:</label> ${years}</p>`);
-document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`); */
+    /* Declare a variable that will be used to store an error message right 
+    after the constant declarations in the processEntries() function. */
+ //  let errorMessage = "";
+    
 
+    /* Add an if-else statement that tests whether each entry is valid.  */
+ //   if (isNaN(investment) || investment <= 0 || investment > 100000) {
+ //       errorMessage = "Investment has to be a number greater than 0 and less than or equal to 100,000";
+ //         $("#investment").focus();
+ //   } 
+ //   else if (isNaN(rate) || rate <= 0 || rate > 15) {
+ //       errorMessage = "Rate has to be a number greater than 0 and less than or equal to 15";
+ //         $("#rate").focus();
+ //  } 
+ //   else if (isNaN(years) || years <= 0 || years > 50) {
+ //       errorMessage = "Years has to be a number greater than 0 and less than or equal to 50";
+ //         $("#years").focus();
+ //   }
 
+    /* 1) Code a statement that calls the calculateFV() function
+       2) if-else statement that tests whether any errors were detected. */
+ //   if (errorMessage == "") {
+ //       $("#future_value").value = calculateFV(investment, rate, years);        
+ //   } 
+ //   else {
+ //  alert(errorMessage);
+ //   }
+//} */
 
-/*============================
-        FOR REFERENCE
-==============================*/
-/* let again = "y";
-do {
-    const miles = parseInt(prompt("Enter miles driven"));
-    const gallons = parseInt(prompt("Enter gallons of gas used"));
+/* Create an event handler for the DOMContentLoaded event  */
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", () => {
+        const investment = parseFloat($("#investment").value);
+        const rate = parseFloat($("#rate").value);
+        const years = parseInt($("#years").value);
 
-    if (miles > 0 && gallons > 0) { 
-        const mpg = parseFloat(miles/gallons);
+        /* Add to this block as well? */
+        let errorMessage = "";
+    
+        /* Add an if-else statement that tests whether each entry is valid. */
+        if (isNaN(investment) || investment <= 0 || investment > 100000) {
+            errorMessage = "Investment has to be a number greater than 0 and less than or equal to 100,000";
+                $("#investment").focus();
+        } 
+        else if (isNaN(rate) || rate <= 0 || rate > 15) {
+            errorMessage = "Rate has to be a number greater than 0 and less than or equal to 15";
+                $("#rate").focus();
+        } 
+        else if (isNaN(years) || years <= 0 || years > 50) {
+            errorMessage = "Years has to be a number greater than 0 and less than or equal to 50";
+                $("#years").focus();
+        }
 
-        const html = `<p>${miles} miles on ${gallons}
-            gallons = ${mpg.toFixed(2)} MPG</p>`;
-        document.write(html); 
-    } 
-    else { 
-        alert("One or both entries are invalid"); 
-    } 
-    again = prompt("Repeat entries? (y/n)", "y"); 
-} 
-while (again == "y"); */
+        /* 1) Code a statement that calls the calculateFV() function
+           2) if-else statement that tests whether any errors were detected. */
+        if (errorMessage == "") {
+            $("#future_value").value = calculateFV(investment, rate, years);        
+        } 
+        else {
+            alert(errorMessage);
+        }
+    });
+    $("#investment").focus();
+});
