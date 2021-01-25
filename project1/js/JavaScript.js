7f7685934f98f34188d6c7c27d8ec6030cba5b59
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-01-24 23:57:30 -0700

    Project1BROKEN
    This is just to meet the submission time. The corrected and finished version will be submitted as "Project1"

---
+++ project1/js/JavaScript.js
@@ -0,0 +1,22 @@
+/* Emma Wood 
+   1-18-2021 
+   File Name: styles.css
+*/
+
+/* Execute the function to run and display 
+the countdown clock */
+runClock();
+setInterval("runClock()", 1000);
+
+/* Function to create and run the countdown clock */
+function runClock() {
+
+    /* Store the current date and time */
+    var currentDay = new Date();
+    var dateStr = currentDay.toLocaleDateString();
+    var timeStr = currentDay.toLocaleTimeString();
+
+    /* Display the current date and time */
+    document.getElementById("timeDate").innerHTML =
+    dateStr + "<br />" + timeStr;
+}
