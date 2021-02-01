95abc57156e773cf30bed5c9c40b44e6d65209d1
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-01 12:16:49 -0700

    Ch8Ex2
    Monday assignment completed on 2-1-2021
    Chapter 8 Exercise 2

--- rollover/rollover.js
+++ rollover/rollover.js
@@ -1,38 +1,22 @@
 "use strict";
+$(document).ready(() => {
 
-const $ = selector => document.querySelector(selector);
+    // process each img tag
+    $("#image_rollovers img").each( (index, img) => {
+        const oldURL = img.src;      // gets the src attribute
+        const newURL = img.id;       // gets the id attribute
 
-document.addEventListener("DOMContentLoaded", () => {
-    var image1 = $("#image1");
-    var image2 = $("#image2");
+        // preload rollover image		
+        const rolloverImage = new Image();
+        rolloverImage.src = newURL;
 
-    // preload images
-    var links = $("#image_list").querySelectorAll("a");
+        $(img).mouseover( () => img.src = newURL );
+        $(img).mouseout( () => img.src = oldURL);
 
-    /* add code that uses the links array to preload the four images  */
-    for (let link of links) {
-        const image = new Image();
-        image.src = link.href;
-    }
-
-    // attach mouseover and mouseout events for each image
-    /* Add code to the mouseover and mouseout event handlers  */
-    for ( let link of links) {
-        image1.addEventListener("mouseover", () => {
-            image1.src = "images/release.jpg";
-        });
-
-        image1.addEventListener("mouseout", () => {
-            image1.src = "images/hero.jpg";
-        });
-
-        image2.addEventListener("mouseover", () => {
-            image2.src = "images/deer.jpg";
-        });
-
-        image2.addEventListener("mouseout", () => {
-            image2.src = "images/bison.jpg";
-        });         
-    }
-
+        // set up event handlers for hovering over an image
+        /* $(img).hover(   // use jQuery syntax to access hover() method
+            () => img.src = newURL,  // hover over
+            () => img.src = oldURL   // hover out
+        ); */
+    });
 });
