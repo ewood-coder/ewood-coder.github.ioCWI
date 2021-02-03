a626baa0feb9ff88e84e93abf95af8ee41c4979e
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-03 11:05:44 -0700

    Ch10Ex1
    Wednesday assignment completed on 2-3-2021
    Chapter 10 Exercise 1

--- email_list/email_list.js
+++ email_list/email_list.js
@@ -1,62 +1,143 @@
-const $ = selector => document.querySelector(selector);
+"use strict";
+$(document).ready( () => {
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    $("#join_list").addEventListener("click", () => {
-        // get values user entered in textboxes
-        const email1 = $("#email_1");
-        const email2 = $("#email_2");
-        const firstName = $("#first_name");
-    
-        /* Use a Boolean variable to keep track of 
-           whether any entries are invalid.  */
-           let isValid = true;
+    // handle click on Join List button
+    $("#join_list").click( evt => {
+        let isValid = true;
 
-        // check user entries - add text to error message if invalid
-        /* Enhance the data validation so it displays the error messages 
-           in the span elements. */
-        if (email1.value == "") {
-            email1.nextElementSibling.textContent = "This field is required.";
-            isValid = false; 
+        // validate the first email address
+        const emailPattern = 
+            /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
+        const email1 = $("#email_1").val().trim();
+        if (email1 == "") { 
+            $("#email_1").next().text("This field is required.");
+            isValid = false;
         } 
-        else {
-            email1.nextElementSibling.textContent = "";
-        }
-        /*=======================================================================*/
-        if (email2.value == "") { 
-            email2.nextElementSibling.textContent = "This field is required.";
-            isValid = false; 
-        } 
-        else {
-            email2.nextElementSibling.textContent = "";
-        }
-        /*=======================================================================*/
-        if (email1.value != email2.value) { 
-            email2.nextElementSibling.textContent = "Email addresses must match.";
-            isValid = false; 
-        }
-        /*=======================================================================*/
-        if (firstName.value == "") {
-            firstName.nextElementSibling.textContent = "First name is required.";
+        else if ( !emailPattern.test(email1) ) {
+            $("#email_1").next().text("Must be a valid email address.");
             isValid = false;
         } 
         else {
-            firstName.nextElementSibling.textContent = "";
+            $("#email_1").next().text("");
+        }
+        $("#email_1").val(email1);
+
+        /*=====================================================================*/
+
+        // validate the second email address
+        const email2 = $("#email_2").val().trim();
+        if (email2 == "") { 
+            $("#email_2").next().text("This field is required.");
+            isValid = false; 
+        } 
+        else if (email1 != email2) { 
+            $("#email_2").next().text("Must equal first email entry.");
+            isValid = false;
+        } 
+        else {
+            $("#email_2").next().text("");
+        }
+        $("#email_2").val(email2);
+
+        /*=====================================================================*/
+        
+        // validate the first name entry  
+        const firstName = $("#first_name").val().trim();
+        if (firstName == "") {
+            $("#first_name").next().text("This field is required.");
+            isValid = false;
+        } 
+        else {
+            $("#first_name").next().text("");
+        }
+        $("#first_name").val(firstName);
+
+        /*=====================================================================*/
+
+        // validate the last name entry
+        const lastName = $("#last_name").val().trim();
+        if (lastName == "") {
+            $("#last_name").next().text("This field is required.");
+            isValid = false;
+        } 
+        else {
+            $("#last_name").next().text("");
+        }
+        $("#last_name").val(lastName);
+
+        /*=====================================================================*/
+
+        // validate the state entry
+		const state = $("#state").val().trim();
+		if (state == "") {
+			$("#state").next().text("This field is required.");
+			isValid = false;
+        } 
+        else if ( state.length != 2 ) {
+			$("#state").next().text("Use 2-character code.");
+			isValid = false;
+        } 
+        else {
+			$("#state").next().text("");
+		}
+        $("#state").val(state);
+        
+        /*=====================================================================*/
+			
+		// validate the zip-code entry
+		const zipCode = $("#zip_code").val().trim();
+		if (zipCode == "") {
+			$("#zip_code").next().text("This field is required.");
+			isValid = false;
+        } 
+        else if (isNaN(zipCode)) {
+            $("#zip_code").next().text("Use 99999 format.");
+            isValid = false;
+        } 
+        else if ( zipCode.length != 5 ) {
+			$("#zip_code").next().text("Use 5-digit code.");
+			isValid = false;
+        } 
+        else {
+			$("#zip_code").next().text("");
+		}
+        $("#zip_code").val(zipCode);
+
+        /*=====================================================================*/
+        			
+        // validate the check boxes	
+        let checkedOptions = [];
+        checkedOptions = $(":checkbox:checked");
+        if (checkedOptions.length == 0) {
+            $("#net").next().text("Select at least one.");
+            isValid = false;
+        } 
+        else {
+            $("#net").next().text("");
         }
 
-        /* if all of the entries are valid, you can submit the form. */
-        if (isValid) {
-            $("#email_form").submit();
-        }
+        /*=====================================================================*/
+						
+		// prevent the default action of submitting the form if any entries are invalid 
+		if (isValid == false) {
+			evt.preventDefault();
+		}
     });
 
-    $("#clear_form").addEventListener("click", () => {
-        $("#email_1").value = "";
-        $("#email_2").value = "";
-        $("#first_name").value = "";
+    // handle click on Reset Form button
+    $("#reset").click( () => {
+        // clear text boxes
+        $("#email_1").val("");
+        $("#email_2").val("");
+        $("#first_name").val("");
 
+        // reset span elements
+        $("#email_1").next().text("*");
+        $("#email_2").next().text("*");
+        $("#first_name").next().text("*");
+        
         $("#email_1").focus();
     });
-    
+    // move focus to first text box
     $("#email_1").focus();
 });
