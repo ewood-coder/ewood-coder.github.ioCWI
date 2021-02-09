3da0155d194f7e02b4921de1427f9d7905e17a77
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-09 14:03:54 -0700

    Ch13Ex2
    completed on 2-9-2021
    Chapter 13 Exercise 2

--- future_value/future_value.js
+++ future_value/future_value.js
@@ -1,126 +1,51 @@
 "use strict";
 
-const getRandomNumber = max => {
-    let rand = null;
-    if (!isNaN(max)) {
-        rand = Math.random();
-        rand = Math.floor(rand * max);
-        rand = rand + 1;
-    }
-    return rand;
-};
-
 const calculateFutureValue = (investment, rate, years) => {
+    if ( isNaN(investment) || investment <= 0 || isNaN(rate) || rate <= 0 || isNaN(years) || years <= 0 ) {
+		// add a throw statement here to make sure the passed arguments are valid
+        throw new RangeError("All entries must be numbers greater than zero.");
+    }
+	
+    // add a throw statement here to test the exception handling
+    /* throw new RangeError("All entries must be numbers greater than zero."); */
+    
     let futureValue = investment;
     for (let i = 1; i <= years; i++ ) {
         futureValue += futureValue * rate / 100;
-        /* Modify the JavaScript by adding an if statement to the for loop 
-           that calculates the future value */
-        if (futureValue == Infinity) {
-            alert ("Future Value = " + futureValue + "\n i = " + i);
-            i = years;
-        }
     }
-    /* Add an alert() method that displays the maximum value of a JavaScript 
-       number after the for loop finishes */
     return futureValue.toFixed(2);
 };
 
-const formatFutureValue = futureValue => {
-    const dotLocation = futureValue.indexOf(".");
-    const cents = futureValue.substring(dotLocation + 1, dotLocation + 3);
-    const hundreds = futureValue.substring(dotLocation - 3, dotLocation);
-    const thousands = "";
-    const millions = "";
-
-    if (dotLocation < 6) {
-        thousands = futureValue.substring(0, dotLocation - 3);
-        millions = "";
-    } else {
-        thousands = futureValue.substring(dotLocation - 6, dotLocation - 3);
-        millions = futureValue.substring(0, dotLocation - 6);
-    }
-
-    let futureValueFormatted = "";
-
-    if (dotLocation >= 7) {
-        futureValueFormatted = "$" + millions + "," + thousands + "," + hundreds + "." + cents;
-    } else {
-        futureValueFormatted = "$" + thousands + "," + hundreds + "." + cents;
-    }
-
-    return futureValueFormatted;
-};
-
-const getDate = futureValue => {
-    const currentDate = new Date();
-
-    let month = currentDate.getMonth() + 1;
-    if (month < 10) {
-        month = "0" + month;
-    }
-
-    let day = currentDate.getDate();
-    if (day < 10) {
-        day = "0" + day;
-    }
-
-    const year = currentDate.getFullYear();
-    const hours = currentDate.getHours();
-
-    let minutes = currentDate.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    const dateString = `Today is ${month}/${day}/${year} at ${hours}:${minutes}.`;	
-    return dateString;    
-}
-
 $(document).ready( () => {
     $("#calculate").click( () => {
-        /* const investment = parseFloat($("#investment").val());
-           const rate = parseFloat($("#rate").val());
-           const years = parseFloat($("#years").val());
-        */
-        const investment = getRandomNumber(50000);
-		$("#investment").val(investment);
+        /* Put all of the statements in this handler except the 
+           last two in the try block */
+        try {
+            $("#message").text("");
+            const investment = parseFloat($("#investment").val());
+            const rate = parseFloat($("#rate").val());
+            const years = parseFloat($("#years").val());
 
-		const rate = getRandomNumber(15);
-		$("#rate").val(rate);
-
-		const years = getRandomNumber(50);
-        $("#years").val(years);
-        
-        let isValid = true;
-        if (isNaN(investment) || investment <= 0) {
-            $("#investment").next().text("Must be a valid number greater than 0.");
-            isValid = false;
-        } else {
-            $("#investment").next().text("");
+            const fv = calculateFutureValue(investment, rate, years);
+            $("#future_value").val(fv);
         }
-
-        if (isNaN(rate) || rate <= 0) {
-            $("#rate").next().text("Must be a valid number greater than 0.");
-            isValid = false;
-        } else {
-            $("#rate").next().text("");
-        }
-
-        if (isNaN(years) || years <= 0) {
-            $("#years").next().text("Must be a valid number greater than 0.");
-            isValid = false;
-        } else {
-            $("#years").next().text("");
-        }
-
-        if (isValid) {
-            const futureValue = calculateFutureValue(investment, rate, years);
-            /* $("#future_value").val(formatFutureValue(futureValue)); */
-            const usCurrency = new Intl.NumberFormat("en-us", {style:"currency", currency:"USD"});
-            $("#future_value").val(usCurrency.format(futureValue));
-
+        catch(error) {
+            $("#message").text( error.name + ": " + error.message );
+        } 
+		finally {
+            $("#investment").focus();
+            $("#investment").select();
         }
     });
-    $("#date").text(getDate());
+
+    $("#clear").click( function() {
+        $("#investment").val("");
+        $("#rate").val("");
+        $("#years").val("");
+        $("#future_value").val("");  
+		$("#message").val("");		
+
+        $("#investment").focus();
+    });
     $("#investment").focus();
 });
