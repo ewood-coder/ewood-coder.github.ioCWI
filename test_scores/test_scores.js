5e820c75d2d0b560cae77a670808675ab15bd5f7
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-11 16:10:38 -0700

    Ch15Ex1
    completed on 2-11-2021
    Chapter 15 Exercise 1

--- test_scores/test_scores.js
+++ test_scores/test_scores.js
@@ -1,30 +1,63 @@
 "use strict";
 
-// initialize total variable
-let total = 0;
+/* Move the code that calculates the average score to a helper function 
+   outside of the ready() event handler */
+const getAvgScore = scores => {
+    const total = scores.reduce( (tot, val) => tot + val, 0 );
+    const avg = total/scores.length;
+    return avg.toFixed(2);
+};
+/* Repeat the previous step for the code that gets the last three scores */
+const getLast3Scores = scores => {
+    let last3Scores = [];
+    const len = scores.length;
+    last3Scores = (len <= 3) ? scores.slice() : scores.slice(len - 3, len);
+    last3Scores.reverse();
+    return last3Scores;
+};
+const updateDisplay = scores => {
+    $("#all").text(scores.join(", "));
+    $("#avg").text(getAvgScore(scores));
+    $("#last").text(getLast3Scores(scores).join(", "));
+}
 
-//get 3 scores from user and add them together
-const score1 = parseInt(prompt("Enter test score"));
-total += score1;
+$(document).ready( () => {
 
-const score2 = parseInt(prompt("Enter test score"));
-total += score2;
+    const scores = [];
 
-const score3 = parseInt(prompt("Enter test score"));
-total += score3;
+    $("#add_score").click( () => {
+        
+        const score = parseFloat($("#score").val());
+                
+        if (isNaN(score) || score < 0 || score > 100) {
+            $("#add_score").next().text("Score must be between 0 and 100."); 
+        }
+        else {
+            $("#add_score").next().text("");  
+            scores.push(score);
+            updateDisplay(scores);
+        }
+        $("#score").val("");
+        $("#score").focus();  
+    });
 
-//calculate the average
-const average = parseInt(total/3);
-
-// display the scores
-/* const html = `<p>Score 1 = ${score1}</p>
-    <p>Score 2 = ${score2}</p>
-    <p>Score 3 = ${score3}</p>
-    <p>Average score = ${average}</p>`;
-document.write(html); */
-
-const html = ("Score 1 = " + score1 + "<br><br>" +
-"Score 2 = " + score2 + "<br><br>" +
-"Score 3 = " + score3 + "<br><br>" +
-"Average score = " + average);
-document.write(html);
+    /* Code the click() event handler for the Delete Score button */
+    $("#delete_score").click( () => {
+        const index = parseInt($("#index").val());
+        /* Add data validation to the click() event handler for the Delete Score button */
+        if (isNaN(index)) {
+            $("#delete_score").next().text("Index must be a whole number.");
+        } 
+        else if (index < 0 || index > scores.length - 1) {
+            $("#delete_score").next().text("There is no element with that index.")
+        } 
+        else {
+            $("#delete_score").next().text("");
+            scores.splice(index, 1);
+            updateDisplay(scores);
+        }
+        $("#index").val("");
+        $("#score").focus();
+    });
+    $("#score").focus();
+});
