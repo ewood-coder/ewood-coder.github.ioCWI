a777f6d05d5facb88bab1cb7c64c30635e1fb870
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-16 10:22:22 -0700

    Ch16Ex1
    Tuesday assignment completed on 2-16-2021
    Chapter 16 Exercise 1

---
+++ countdown/lib_validation.js
@@ -0,0 +1,18 @@
+const validation = {
+    isEmpty(val) {
+        return (val === "");
+    },
+
+    hasNoSlashes(val) {
+        return (val.indexOf("/") === -1);
+    },
+
+    isInvalidYear(val) {
+        const year = val.substring(val.length - 4); 
+        return (isNaN(year)); 
+    },
+    
+    isInvalidDate(val) {
+        return (val.toString() === "Invalid Date");
+    }
+};
