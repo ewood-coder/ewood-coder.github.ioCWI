81b4852aa49acaa4cd69b8d60acadc51b915944b
ewood-coder <73657745+ewood-coder@users.noreply.github.com>
2021-02-17 12:56:53 -0700

    Ch17Ex1
    completed on 2-17-2021
    Chapter 17 Exercise 1

--- test_scores/test_scores.js
+++ test_scores/test_scores.js
@@ -1,31 +1,47 @@
 "use strict";
 
+/* Modify the isInvalid(), getAverage(), and getLast() functions so 
+   they use a function declaration instead of an arrow function */
+function isInvalid(score) {
+    return isNaN(score) || score < 1 || score > 100;
+}
+/* Modify the getAverage() function so it uses a default value */
+function getAverage(arr, places = 2) {
+    const total = arr.reduce( (tot, val) => tot + val, 0 );
+    const average = total/arr.length;
+    return average.toFixed(places);
+};
+function getLast(arr, num = 3) {
+    const copy = [...arr];
+    copy.reverse();
+    return copy.slice(0, num);
+}
+
 $(document).ready( () => {
-    $("#add_score").click( () => {
+
+    const scores = [];
+
+    /* Modify the click() event handler so it uses an arrow function 
+       instead of a function expression */
+       $("#add_score").click( () => {
         const score = parseFloat($("#score").val());
+                
+        if (isInvalid(score)) { 
+            $("#add_score").next().text("Score must be between 1 and 100.");
+        }
+        else {  
+            $("#add_score").next().text("");
 
-        if (isNaN(score) || score < 0 || score > 100) {
-            $("#add_score").next().text("Score must be between 0 and 100."); 
+            scores.push(score);
+
+            $("#high").text(Math.max(...scores));
+            $("#low").text(Math.min(...scores));
+            $("#last").text(getLast(scores).join(", "));
+            $("#avg").text(getAverage(scores, 1));
+            $("#all").text(scores.join(", "));
         }
-        else {
-            $("#add_score").next().text("");  
-            // add score to scores array 
-            testScores.add(score);
-            // display all scores
-            $("#all").text(testScores.toString());
-            // display average score
-            $("#avg").text(testScores.avg.toFixed(1));
-            // display last 3 scores
-            $("#last").text(testScores.lastThree);
-        }
-        // get text box ready for next entry
         $("#score").val("");
-        $("#score").focus(); 
-
-        for (const score of testScores) {
-            console.log(score);
-        }
+        $("#score").focus();
     });
-    // set focus on initial load
     $("#score").focus();
 });
